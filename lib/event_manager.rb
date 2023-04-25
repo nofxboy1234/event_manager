d4ecd525a323397ffa7791bff8b726c3af31077f
Dylan Palmboom <nofxboy1234@gmail.com>
2023-04-25 19:32:33 +0200

    Create #save_thank_you_letter

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -21,6 +21,16 @@ def legislators_by_zipcode(zip)
   end
 end
 
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts 'Event Manager Initialized'
 
 contents = CSV.open(
@@ -35,18 +45,10 @@ erb_template = ERB.new template_letter
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
-  Dir.mkdir('output') unless Dir.exist?('output')
-
-  filename = "output/thanks_#{id}.html"
-
-  File.open(filename, 'w') do |file|
-    file.puts form_letter
-  end
+  save_thank_you_letter(id, form_letter)
 end
