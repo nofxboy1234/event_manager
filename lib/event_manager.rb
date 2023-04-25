6e72687983bef61221615f15dc8bc6a2578760de
Dylan Palmboom <nofxboy1234@gmail.com>
2023-04-25 13:56:40 +0200

    Remove row_index

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,10 @@
 puts 'Event Manager Initialized!'
 
-# if File.exist? 'event_attendees.csv'
-#   contents = File.read('event_attendees.csv')
-#   puts contents
-# end
-
 lines = File.readlines('event_attendees.csv')
-row_index = 0
 lines.each_with_index do |line, index|
   next if index == 0
   columns = line.split(",")
   name = columns[2]
   puts name
+
 end
