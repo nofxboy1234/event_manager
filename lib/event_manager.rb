8303303a0c38853dd76ac7ad1ce819a1b792ab35
Dylan Palmboom <nofxboy1234@gmail.com>
2023-04-25 13:55:24 +0200

    Use each_with_index

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,9 +7,8 @@ puts 'Event Manager Initialized!'
 
 lines = File.readlines('event_attendees.csv')
 row_index = 0
-lines.each do |line|
-  row_index = row_index + 1
-  next if row_index == 1
+lines.each_with_index do |line, index|
+  next if index == 0
   columns = line.split(",")
   name = columns[2]
   puts name
