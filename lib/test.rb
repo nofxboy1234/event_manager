734bbd51ce8e55cb079355951a2c190f31e1a4b2
Dylan Palmboom <nofxboy1234@gmail.com>
2023-04-28 08:58:32 +0200

    Update serialization tests

--- lib/test.rb
+++ lib/test.rb
@@ -1,7 +1,6 @@
 require 'yaml'
 require 'json'
 
-
 class Person
   attr_accessor :name, :age, :gender
 
@@ -16,6 +15,9 @@ class Person
       :name => @name,
       :age => @age,
       :gender => @gender
+      # 'name' => @name,
+      # 'age' => @age,
+      # 'gender' => @gender
     })
   end
 
@@ -24,35 +26,68 @@ class Person
       :name => @name,
       :age => @age,
       :gender => @gender
+      # 'name' => @name,
+      # 'age' => @age,
+      # 'gender' => @gender
+
     })
   end
+  
+  def self.from_yaml(string)
+    p string
+    data = YAML.load string
+    p data
+    self.new(data[:name], data[:age], data[:gender])
+    # self.new(data['name'], data['age'], data['gender'])
+  end
 
-  def yaml_to_file(string)
+  def self.from_json(string)
+    p string
+    data = JSON.load string
+    p data
+    self.new(data['name'], data['age'], data['gender'])
+    # self.new(data[:name], data[:age], data[:gender]) # Does not work. Key must be string
+  end
+
+  def self.yaml_to_file(string)
     filename = "yaml_output.yaml"
   
     File.open(filename, 'w') do |file|
       file.puts string
     end
   end
-
-  def self.from_yaml(string)
-    p string
-    data = YAML.load string
-    p data
-    self.new(data[:name], data[:age], data[:gender])
-  end
-
-  def self.from_json(string)
-    data = JSON.load string
-    self.new(data['name'], data['age'], data['gender'])
+  
+  def self.json_to_file(string)
+    filename = "json_output.json"
+  
+    File.open(filename, 'w') do |file|
+      file.puts string
+    end
   end
 end
 
 p = Person.new "David", 28, "male"
-# p p.to_yaml
 
 p = Person.from_yaml(p.to_yaml)
 
 puts "Name #{p.name}"
 puts "Age #{p.age}"
 puts "Gender #{p.gender}"
+
+Person.yaml_to_file(p.to_yaml)
+
+p YAML.load File.read('yaml_output.yaml')
+
+
+
+# p = Person.new "David", 28, "male"
+
+# p = Person.from_json(p.to_json)
+
+# puts "Name #{p.name}"
+# puts "Age #{p.age}"
+# puts "Gender #{p.gender}"
+
+# Person.json_to_file(p.to_json)
+
+# p JSON.load File.read('json_output.json')
