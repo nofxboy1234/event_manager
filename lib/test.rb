3ce958db798de413e10087f225f866747198630e
Dylan Palmboom <nofxboy1234@gmail.com>
2023-04-28 08:23:51 +0200

    Update yaml test

--- lib/test.rb
+++ lib/test.rb
@@ -1,6 +1,7 @@
 require 'yaml'
 require 'json'
 
+
 class Person
   attr_accessor :name, :age, :gender
 
@@ -10,14 +11,6 @@ class Person
     @gender = gender
   end
 
-  # def to_yaml
-  #   YAML.dump ({
-  #     'name' => @name,
-  #     'age' => @age,
-  #     'gender' => @gender
-  #   })
-  # end
-
   def to_yaml
     YAML.dump ({
       :name => @name,
@@ -25,20 +18,6 @@ class Person
       :gender => @gender
     })
   end
-
-  def self.from_yaml(string)
-    data = YAML.load string
-    p data
-    self.new(data[:name], data[:age], data[:gender])
-  end
-
-  # def to_json
-  #   JSON.dump ({
-  #     'name' => @name,
-  #     'age' => @age,
-  #     'gender' => @gender
-  #   })
-  # end
 
   def to_json
     JSON.dump ({
@@ -48,28 +27,32 @@ class Person
     })
   end
 
+  def yaml_to_file(string)
+    filename = "yaml_output.yaml"
+  
+    File.open(filename, 'w') do |file|
+      file.puts string
+    end
+  end
+
+  def self.from_yaml(string)
+    p string
+    data = YAML.load string
+    p data
+    self.new(data[:name], data[:age], data[:gender])
+  end
+
   def self.from_json(string)
     data = JSON.load string
-    p data
     self.new(data['name'], data['age'], data['gender'])
   end
 end
 
 p = Person.new "David", 28, "male"
-p p.to_yaml
+# p p.to_yaml
 
 p = Person.from_yaml(p.to_yaml)
+
 puts "Name #{p.name}"
 puts "Age #{p.age}"
 puts "Gender #{p.gender}"
-
-
-# p = Person.new "David", 28, "male"
-# p p.to_json
-
-# p = Person.from_json(p.to_json)
-# puts "Name #{p.name}"
-# puts "Age #{p.age}"
-# puts "Gender #{p.gender}"
-
-
